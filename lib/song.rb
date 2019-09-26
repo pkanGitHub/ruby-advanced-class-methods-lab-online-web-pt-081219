2c96018e7b53bfc2ae14171714ce6801af29dec6
Phyllis Kan <pyk9q7@gmail.com>
2019-09-26 19:57:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,17 +22,21 @@ class Song
     song
   end
 
-  def self.find_by_name(song_name)
+  def self.create_by_name(song_name)
     song = self.create
     song.name = song_name
     song
+  end
+
+  def self.find_by_name(song_name)
+    
   end
 
   def self.find_or_create_by_name
 
   end
 
-  def self. alphabetical
+  def self.alphabetical
 
   end
 
