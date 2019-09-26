a4d2e9c95bd56fb922ad77bf7adaf0fb36efa607
Phyllis Kan <pyk9q7@gmail.com>
2019-09-26 21:26:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
     song
   end
 
-  def self.create_from_filename
+  def self.create_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
